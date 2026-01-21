049c8a0ff8b387a8a105da63cc6af164396d7c0d
Kendo <s6701012610376@email.kmutnb.ac.th>
2026-01-21 20:09:50 +0700

    add priority feature

--- lists/models.py
+++ lists/models.py
@@ -6,5 +6,21 @@ class List(models.Model):
     pass
 
 class Item(models.Model):
+    PRIORITY_HIGH = "H"
+    PRIORITY_MED = "M"
+    PRIORITY_LOW = "L"
+
+    PRIORITY_CHOICES = [
+        (PRIORITY_HIGH, "High"),
+        (PRIORITY_MED, "Medium"),
+        (PRIORITY_LOW, "Low"),
+    ]
+
     text = models.TextField(default="")
-    list = models.ForeignKey(List, default=None, on_delete=models.CASCADE)
+    list = models.ForeignKey(List, on_delete=models.CASCADE)
+
+    priority = models.CharField(
+        max_length=1,
+        choices=PRIORITY_CHOICES,
+        default=PRIORITY_MED,
+    )
--- lists/views.py
+++ lists/views.py
@@ -13,19 +13,29 @@ def about_page(request) :
 
 def new_list(request):
     nulist = List.objects.create()
-    Item.objects.create(text=request.POST["item_text"], list=nulist)
+    Item.objects.create(
+        text=request.POST["item_text"],
+        list=nulist,
+        priority=request.POST.get("priority", Item.PRIORITY_MEDIUM),
+    )
     return redirect(f"/lists/{nulist.id}/")
 
 def view_list(request, list_id):
     our_list = List.objects.get(id=list_id)
-    return render(request, "list.html", {"list": our_list})
+    items = our_list.item_set.order_by("priority")  # H, L, M order depends on letters
+    return render(request, "list.html", {"list": our_list, "items": items})
+
 
 def add_item(request, list_id):
     pass
 
 def add_item(request, list_id):
     our_list = List.objects.get(id=list_id)
-    Item.objects.create(text=request.POST["item_text"], list=our_list)
+    Item.objects.create(
+        text=request.POST["item_text"],
+        list=our_list,
+        priority=request.POST.get("priority", Item.PRIORITY_MEDIUM),
+    )
     return redirect(f"/lists/{our_list.id}/")
 
     
