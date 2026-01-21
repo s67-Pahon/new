99cf9dc897c3bed9a3a14b0df53afcadd5248a81
Kendo <s6701012610376@email.kmutnb.ac.th>
2026-01-21 20:12:44 +0700

    fix models.py priority nedium

--- lists/models.py
+++ lists/models.py
@@ -1,26 +1,25 @@
 from django.db import models
 
 # Create your models here.
-
 class List(models.Model):
     pass
 
+
 class Item(models.Model):
     PRIORITY_HIGH = "H"
-    PRIORITY_MED = "M"
+    PRIORITY_MEDIUM = "M"
     PRIORITY_LOW = "L"
 
     PRIORITY_CHOICES = [
         (PRIORITY_HIGH, "High"),
-        (PRIORITY_MED, "Medium"),
+        (PRIORITY_MEDIUM, "Medium"),
         (PRIORITY_LOW, "Low"),
     ]
 
     text = models.TextField(default="")
     list = models.ForeignKey(List, on_delete=models.CASCADE)
-
     priority = models.CharField(
         max_length=1,
         choices=PRIORITY_CHOICES,
-        default=PRIORITY_MED,
+        default=PRIORITY_MEDIUM,
     )
